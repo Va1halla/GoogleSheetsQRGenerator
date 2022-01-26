83ab532d1c146c89f461d869162122e103ab7439
Home <Home@VALHALLADESK>
2022-01-26 12:30:13 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(26, 366);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 77);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(27, 497);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(27, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сгенерировать QR-код";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 315);
+            this.label1.MaximumSize = new System.Drawing.Size(300, 50);
+            this.label1.MinimumSize = new System.Drawing.Size(300, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 50);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "\"Введите ссылку на таблицу (пример: https://docs.google.com/spreadsheets/d/1BxiMV" +
+    "s0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit#gid=0)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 446);
+            this.label2.MaximumSize = new System.Drawing.Size(300, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(299, 48);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите диапазон строк через дефис, указывая буквы ЛАТИНИЦЕЙ (пример: A1-22 или A" +
+    "1-B1 )\"";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(29, 583);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(299, 52);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Сохранить QR-код";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 647);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(361, 694);
+            this.MinimumSize = new System.Drawing.Size(361, 694);
+            this.Name = "Form1";
+            this.Text = "QR APP";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const string PREFIX = "&range=";
+            const string pattern1 = "[a-zA-Z][0-9]+-[0-9]+";
+            const string pattern2 = "[a-zA-Z][0-9]+-[a-zA-Z][0-9]+";
+            string PrettyLink = "";
+            string PrettyRange = "";
+            string RawLink = textBox1.Text;
+            string RawRange = textBox2.Text;
+            var regex1 = Regex.Match(RawRange, pattern1);
+            var regex2 = Regex.Match(RawRange, pattern2);
+            bool succsessFlag = false;
+            //Regex1 - A11-12
+            if (regex1.Success)
+            {
+                string range = regex1.Value;
+                const string ColumnPattern = "[a-zA-Z]";
+                const string RowPattern = "[0-9]+";
+                string column = Regex.Match(range, ColumnPattern).Value;
+                var rows = Regex.Matches(range, RowPattern);
+                foreach (var row in rows)
+                {
+                    PrettyRange += column + row + ":";
+                }
+                succsessFlag = true;
+            }
+            //Regex2 - A11-A12
+            else if (regex2.Success)
+            {
+                const string CellPattern = "[a-zA-Z][0-9]+";
+                var cells = Regex.Matches(RawRange, CellPattern);
+                foreach (var cell in cells)
+                {
+                    PrettyRange += cell + ":";
+                }
+                succsessFlag = true;
+            }
+            else
+            {
+                MessageBox.Show("Неверный диапазон ячеек!");
+                textBox2.Clear();
+            }
+            if (succsessFlag == true)
+            {
+                PrettyRange = PrettyRange.TrimEnd(':');
+                PrettyLink += RawLink + PREFIX + PrettyRange;
+                //Generate QR-code
+                QRCodeEncoder encoder = new QRCodeEncoder();
+                encoder.QRCodeScale = 4;
+                Bitmap bitmap = encoder.Encode(PrettyLink);
+                pictureBox1.Image = bitmap as Image;
+
+                button2.Enabled = true;
+                pictureBox1.Enabled = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog(); 
+            save.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp";
+            if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image.Save(save.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                SaveFileDialog save = new SaveFileDialog(); 
+                save.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp"; 
+                if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
+                {
+                    pictureBox1.Image.Save(save.FileName);
+                }
+            }
+        }
+    }
+}
